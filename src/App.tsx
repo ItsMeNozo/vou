3e89919ab97e5bba865bdd48e2b0a27c844e118e
ItsMeNozo <dungtuyetngoc@gmail.com>
2024-07-27 22:12:00 +0700

    feat: implement main game screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,53 @@ import EventList from './pages/EventList/EventList.tsx';
 import VoucherList from './pages/VoucherList/VoucherList.tsx';
 import Navbar from './components/Navbar.tsx';
 import './App.css';
+import MainScreen from './pages/Game/MainScreen.tsx';
 
 const Layout = () => {
-	return (
-		<>
-			<Outlet />
-			<Navbar />
-		</>
-	);
+  return (
+    <>
+      <Outlet />
+      <Navbar />
+    </>
+  );
 };
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Layout />,
-		children: [
-			{
-				path: '/',
-				element: <EventList />,
-			},
-			{
-				path: '/discover',
-				element: <EventList />,
-			},
-			{
-				path: '/voucher',
-				element: <VoucherList />,
-			},
-		],
-	},
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <EventList />,
+      },
+      {
+        path: '/discover',
+        element: <EventList />,
+      },
+      {
+        path: '/voucher',
+        element: <VoucherList />,
+      },
+    ],
+  },
+  {
+    path: 'game',
+    children: [
+      {
+        path: 'main',
+        element: <MainScreen />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <div>Page not found</div>,
+  },
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Game/MainScreen.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import classes from './MainScreen.module.css';
+
+import inventoryImg from '@/assets/game/inventory.png';
+import item1 from '@/assets/game/items/item1.png';
+import item2 from '@/assets/game/items/item2.png';
+import item3 from '@/assets/game/items/item3.png';
+import shake from '@/assets/game/wshake.png';
+
+const MainScreen: React.FC = () => {
+  return (
+    <div>
+      <nav className={classes.navbar}>
+        <Link to={'#info'} className={`${classes.navlink} ${classes.profile}`}>
+          <span className={classes.username}>Ngoc Pham</span>
+          <small className={classes.remaining}>3 plays remaining</small>
+        </Link>
+        <div className={classes.navbar_controls}>
+          <Link to={'#notification'} className={classes.navlink}>
+            <span className={`material-symbols-outlined`}>notifications</span>
+          </Link>
+          <Link to={'#friend'} className={`${classes.navlink}`}>
+            <span className="material-symbols-outlined">group</span>
+          </Link>
+          <Link to={'#inventory'} className={classes.navlink}>
+            <img src={inventoryImg} alt="inventory" width="24" />
+          </Link>
+          <Link to={'#help'} className={`${classes.navlink}`}>
+            <span className={`material-symbols-outlined ${classes.help}`}>
+              help
+            </span>
+          </Link>
+        </div>
+      </nav>
+      <div
+        className={classes.main_screen}
+        style={{
+          background:
+            'linear-gradient(352.71deg, #6530b9 -0.26%, #FFFFFF 73.24%)',
+        }}
+      >
+        <div className={classes.game_menu}>
+          <h1 className={classes.game_title}>Shake the pet</h1>
+          <div className={classes.game_banner}>
+            <img src={item1} alt="Game Icon" width={100} />
+            <img src={item2} alt="Game Icon" width={100} />
+            <img src={item3} alt="Game Icon" width={100} />
+          </div>
+          <button className={classes.start_btn}>SHAKE IT</button>
+          {/* TODO: add this instruction below shaking ball
+          <div className={classes.instruction}>
+            <img
+              src={shake}
+              alt="Game Icon"
+              width={120}
+              className={classes.shake_image}
+            />
+            <p>
+              Shake the pet to collect items. Collect enough items to win
+              rewards.
+            </p>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainScreen;
