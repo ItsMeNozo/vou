20c77d5d1c82626572da48a6b79ee81ad86049e4
unknown <dungtuyetngoc@gmail.com>
2024-07-27 22:42:33 +0700

    feature/add treasure chest shaking component

---
+++ src/pages/TestPage/TestPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Box from "@/components/ShakingBox";
+
+const TestPage: React.FC = () => {
+  return (
+    <div className="">
+      <div className="flex flex-col items-center">
+        <Box />
+      </div>
+    </div>
+  );
+};
+
+export default TestPage;
---
+++ src/shake.d.ts
@@ -0,0 +1,14 @@
+declare module "shake.js" {
+  interface ShakeOptions {
+    threshold?: number;
+    timeout?: number;
+  }
+
+  class Shake {
+    constructor(options?: ShakeOptions);
+    start(): void;
+    stop(): void;
+  }
+
+  export default Shake;
+}
