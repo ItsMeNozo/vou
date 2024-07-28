ab781a8213fd66c514b994a3de8b94b469621fcf
unknown <dungtuyetngoc@gmail.com>
2024-07-28 08:35:02 +0700

    fix/deploy fail due to shake declared never used

--- src/pages/Game/MainScreen.tsx
+++ src/pages/Game/MainScreen.tsx
@@ -1,44 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import classes from './MainScreen.module.css';
+import classes from "./MainScreen.module.css";
 
-import inventoryImg from '@/assets/game/inventory.png';
-import item1 from '@/assets/game/items/item1.png';
-import item2 from '@/assets/game/items/item2.png';
-import item3 from '@/assets/game/items/item3.png';
-import shake from '@/assets/game/wshake.png';
+import inventoryImg from "@/assets/game/inventory.png";
+import item1 from "@/assets/game/items/item1.png";
+import item2 from "@/assets/game/items/item2.png";
+import item3 from "@/assets/game/items/item3.png";
 
 const MainScreen: React.FC = () => {
   return (
     <div>
       <nav className={classes.navbar}>
-        <Link to={'#info'} className={`${classes.navlink} ${classes.profile}`}>
+        <Link to={"#info"} className={`${classes.navlink} ${classes.profile}`}>
           <span className={classes.username}>Ngoc Pham</span>
           <small className={classes.remaining}>3 plays remaining</small>
         </Link>
         <div className={classes.navbar_controls}>
-          <Link to={'#notification'} className={classes.navlink}>
+          <Link to={"#notification"} className={classes.navlink}>
             <span className={`material-symbols-outlined`}>notifications</span>
           </Link>
-          <Link to={'#friend'} className={`${classes.navlink}`}>
+          <Link to={"#friend"} className={`${classes.navlink}`}>
             <span className="material-symbols-outlined">group</span>
           </Link>
-          <Link to={'#inventory'} className={classes.navlink}>
+          <Link to={"#inventory"} className={classes.navlink}>
             <img src={inventoryImg} alt="inventory" width="24" />
           </Link>
-          <Link to={'#help'} className={`${classes.navlink}`}>
-            <span className={`material-symbols-outlined ${classes.help}`}>
-              help
-            </span>
+          <Link to={"#help"} className={`${classes.navlink}`}>
+            <span className={`material-symbols-outlined ${classes.help}`}>help</span>
           </Link>
         </div>
       </nav>
       <div
         className={classes.main_screen}
         style={{
-          background:
-            'linear-gradient(352.71deg, #6530b9 -0.26%, #FFFFFF 73.24%)',
+          background: "linear-gradient(352.71deg, #6530b9 -0.26%, #FFFFFF 73.24%)",
         }}
       >
         <div className={classes.game_menu}>
