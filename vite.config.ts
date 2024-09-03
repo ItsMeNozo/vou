53f12cae7cd36ee9c0f879179a026c4a287fab5b
ItsMeNozo <dungtuyetngoc@gmail.com>
2024-09-03 22:39:40 +0700

    feat: change port

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  server: {
+    port: 5171,
+  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
