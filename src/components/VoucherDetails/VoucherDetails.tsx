2f68b45003c6694b10686a5a47608a8d1ac1a3a4
999Kit <98326962+999Kit@users.noreply.github.com>
2024-09-10 13:52:18 +0700

    feat: voucher list of user + voucher details page

--- src/components/VoucherDetails/VoucherDetails.tsx
+++ src/components/VoucherDetails/VoucherDetails.tsx
@@ -3,90 +3,92 @@ import { MdArrowBackIos } from "react-icons/md";
 import QRCode from 'react-qr-code';
 import voucherImgUrl from '@/assets/500k.png';
 import { useNavigate } from "react-router-dom";
-import Voucher, {VoucherStatus, RedeemMethod} from "@/types/Voucher";
+import Voucher, { VoucherStatus, RedeemMethod } from "@/types/Voucher";
+import { useLocation } from "react-router-dom";
+
 
 interface VoucherDetailsProps {
 }
 
-
-
-
-
-const defaultVoucher: Voucher = {
-  QRCode: "111222333444",
-  voucherCode: "111222333444",
-  imgUrl: voucherImgUrl,
-  value: '100000',
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus fuga neque illo hic eligendi ea explicabo delectus, libero veniam quasi aspernatur voluptates consectetur distinctio praesentium tempora fugiat recusandae reiciendis asperiores!',
-  expiryDt: new Date("2025-03-03"),
-  status: VoucherStatus.Ongoing,
-  redeemMethod: RedeemMethod.Online,
-  brand: 'Cool Drinks Inc.'
-}
-
 const VoucherDetails: React.FC<VoucherDetailsProps> = () => {
+  const location = useLocation();
+  console.log(location.state); // Add this to check what's being passed
+
+  // Access the passed voucher object from state
+  const { voucherDetails } = location.state || {};
+
+
+  if (!voucherDetails) {
+    return <div>No voucher found</div>;
+  }
+
   const navigate = useNavigate();
-  const [voucher, setVoucher] = useState<Voucher>(defaultVoucher)
+  const [voucher, setVoucher] = useState<any>(voucherDetails)
 
   return (
     <div className=' bg-slate-200 mb-16'>
-      <div className="absolute p-2 top-6 left-6 rounded-full bg-zinc-400 bg-opacity-75 flex flex-col justify-center items-center "
-        onClick={() => navigate(-1)}
-      >
-        <MdArrowBackIos className="text-center" />
-      </div>
 
-      <div className="text-left p-4 sticky top-0 z-50 bg-white shadow-md">
+
+      <div className="text-left p-4 sticky top-0 z-50 bg-white shadow-md flex flex-row">
+        <div className=" flex flex-col justify-center items-center mr-2 "
+          onClick={() => navigate(-1)}
+        >
+          <MdArrowBackIos className="text-center" />
+        </div>
         <span className="text-left text-2xl font-bold">Voucher Details</span>
       </div>
 
       <div className="w-full h-[300px] bg-white">
-        <img src={voucher.imgUrl} className="w-full h-full object-contain" />
+        <img src={voucher.voucher.imgUrl} className="w-full h-full object-contain" />
       </div>
 
       <div className="bg-white mt-2 mx-2 p-4 text-left rounded">
-        <p className="font-bold text-lg">{voucher.brand}</p>
+        <p className="font-bold text-lg">{voucher.voucher.saleEvent.brandName}</p>
       </div>
 
       <div className=" h-[300px] flex flex-col justify-center items-center bg-white mt-2 mx-2 rounded">
         <div className="p-5 rounded-xl bg-white text-center border-4 border-black">
           <QRCode
-            value={voucher.QRCode}
+            value={voucher.voucher.qrCode}
             bgColor={'#FFFFFF'}
             fgColor={'#000000'}
             size={170}
           />
         </div>
         <div className="mt-3 font-bold">
-          QR code: {voucher.QRCode}
+          QR code: {voucher.voucher.qrCode}
         </div>
 
       </div>
       <div className="bg-white p-4 rounded max-w-md mx-2 mt-2">
         <div className="flex justify-between border-b py-2">
           <span className="font-semibold">Voucher Code:</span>
-          <span>{defaultVoucher.voucherCode}</span>
+          <span>{voucher.voucher.code}</span>
         </div>
         <div className="flex justify-between border-b py-2">
           <span className="font-semibold">Value:</span>
-          <span>{defaultVoucher.value}</span>
+          <span>{voucher.voucher.value}</span>
         </div>
         <div className="flex justify-between border-b py-2">
           <span className="font-semibold">Expiry Date:</span>
-          <span>{defaultVoucher.expiryDt.toLocaleDateString("en-GB")}</span>
+          <span>{new Date(voucher.voucher.expiryDt).toLocaleDateString("en-GB", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })}</span>
         </div>
         <div className="flex justify-between border-b py-2">
           <span className="font-semibold">Status:</span>
-          <span>{defaultVoucher.status}</span>
+          <span>{voucher.status}</span>
         </div>
         <div className="flex justify-between py-2">
           <span className="font-semibold">Redeem Method:</span>
-          <span>{defaultVoucher.redeemMethod}</span>
+          <span>{voucher.voucher.redeemMethod}</span>
         </div>
       </div>
       <div className="bg-white mt-2 mx-2 p-4 text-left rounded">
         <p className="text-xl font-bold mb-2">Description</p>
-        <p className="text-sm">{voucher.description}</p>
+        <p className="text-sm">{voucher.voucher.description}</p>
       </div>
     </div>
   )
