56c3a823024a7afd5003d4af72d457fc7d6b0af3
liwjln <phamtrantuantu17@gmail.com>
2024-09-10 17:40:40 +0700

    chore: add api_gateway_url into quiz game

--- src/providers/SocketProvider.tsx
+++ src/providers/SocketProvider.tsx
@@ -1,5 +1,12 @@
 import React, { createContext, useEffect, useState } from "react";
-import { io, Socket } from "socket.io-client"; 
+import { io, Socket } from "socket.io-client";
+
+// Access the API Gateway URL from environment variables
+const API_GATEWAY_URL = import.meta.env.VITE_API_GATEWAY_URL;
+
+if (!API_GATEWAY_URL) {
+  throw new Error("API_GATEWAY_URL is not defined in environment variables");
+}
 
 export const SocketContext = createContext<Socket | null>(null);
 
@@ -9,7 +16,7 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3000");
+    const newSocket = io(`http://localhost:3003`);
     setSocket(newSocket);
 
     return () => {
